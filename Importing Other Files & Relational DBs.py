9c283ea0af31a59786e00c8041da0d87469ed448
Sukriti Shukla <sukritishukla@gmail.com>
2020-12-09 16:54:39 +0000

    Working with SQL in Python

--- Importing Other Files & Relational DBs.py
+++ Importing Other Files & Relational DBs.py
@@ -16,6 +16,17 @@ print(d)
 # Print datatype of d
 print(type(d))
 
+# Import package
+import scipy.io
+
+# Load MATLAB file: mat
+mat = scipy.io.loadmat('albeck_gene_expression.mat')
+
+# Print the datatype type of mat
+print(type(mat))
+
+
+
 # Import pandas
 import pandas as pd
 
@@ -97,3 +108,102 @@ plt.xlabel('GPS Time (s)')
 plt.ylabel('strain')
 plt.show()
 
+# Print the keys of the MATLAB dictionary
+print(mat.keys())
+
+# Print the type of the value corresponding to the key 'CYratioCyt'
+print(type(mat['CYratioCyt']))
+
+# Print the shape of the value corresponding to the key 'CYratioCyt'
+print(mat['CYratioCyt'].shape)
+
+# Subset the array and plot it
+data = mat['CYratioCyt'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
+
+## Relational Database in Python -
+# Import necessary module
+from sqlalchemy import create_engine
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Import necessary module
+from sqlalchemy import create_engine
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Save the table names to a list: table_names
+table_names = list(engine.table_names())
+
+# Print the table names to the shell
+print(table_names)
+
+# Querying relational databases in Python
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine connection: con
+con = engine.connect()
+
+# Perform query: rs
+rs = con.execute('SELECT * FROM Album')
+
+# Save results of the query to DataFrame: df
+df = pd.DataFrame(rs.fetchall())
+
+# Close connection
+con.close()
+
+# Print head of DataFrame df
+print(df.head())
+
+# Open engine in context manager
+with engine.connect() as con:
+    rs = con.execute("SELECT LastName, Title FROM Employee")
+    df = pd.DataFrame(rs.fetchmany(size=3))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Use WHERE Clause -
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Employee WHERE EmployeeId >=6")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Use ORDER BY -
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Employee ORDER BY BirthDate")
+    df = pd.DataFrame(rs.fetchall())
+
+# Set the DataFrame's column names
+df.columns = rs.keys()
+
+# Print head of DataFrame
+print(df.head())
+
+# Querying relational databases directly with pandas -
