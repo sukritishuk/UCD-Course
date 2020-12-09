dee21ce3658b9fc245b55b4bed24461fe0951c13
Sukriti Shukla <sukritishukla@gmail.com>
2020-12-09 16:58:57 +0000

    Working with SQL in Python

--- Importing Other Files & Relational DBs.py
+++ Importing Other Files & Relational DBs.py
@@ -207,3 +207,40 @@ df.columns = rs.keys()
 print(df.head())
 
 # Querying relational databases directly with pandas -
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM Album",engine)
+
+# Print head of DataFrame
+print(df.head())
+
+# Open engine in context manager and store query result in df1
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Album")
+    df1 = pd.DataFrame(rs.fetchall())
+    df1.columns = rs.keys()
+
+# Confirm that both methods yield the same result
+print(df.equals(df1))
+
+# Advanced querying: exploiting table relationships
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist ON  Album.ArtistID = Artist.ArtistID")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000",engine)
+
+# Print head of DataFrame
+print(df.head())
