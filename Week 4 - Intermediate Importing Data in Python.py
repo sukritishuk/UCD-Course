a533c7e7754403671dcaab7c55feebc3b6ad3053
Sukriti Shukla <sukritishukla@gmail.com>
2020-12-11 19:27:40 +0000

    Importing Data from Web & Interaction with APIs

--- Week 4 - Intermediate Importing Data in Python.py
+++ Week 4 - Intermediate Importing Data in Python.py
@@ -114,3 +114,67 @@ a_tags = soup.find_all('a')
 # Print the URLs to the shell
 for link in a_tags:
     print(link.get('href'))
+
+
+
+# Introduction to APIs and JSONs -
+import json
+with open('snake.json','r') as json_file:
+    json_data = json.load(json_file)
+type(json_data)
+
+# Example 2 -
+with open(r'D:\Learning\UCD Professional Academy\Course 1 - Professional Certificate in Data Analytics\Python Practice\world_countries.json') as file:
+    data = json.load(file)
+print(type(data))
+
+
+## APIs and interacting with the world wide web -
+# OMDB Movie API
+import requests
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=the+social+network'
+# Package the request, send the request and catch the response:
+r = requests.get(url)
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+for key,value in json_data.items():
+    print(key + ':',value)
+print(r.text)
+
+# Checking out the Wikipedia API - (Wikipedia page for Pizza)
+import requests
+url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza'
+# Package the request, send the request and catch the response:
+r = requests.get(url)
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+# Print the Wikipedia page extract
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
+
+
+# The Twitter API and Authentication -
+# Example -
+# Import package
+import tweepy
+
+# Store OAuth authentication credentials in relevant variables
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler('nZ6EA0FxZ293SxGNg8g8aP0HM','fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i' )
+
+auth.set_access_token("1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy","X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx")
+
+## Streaming tweets -
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create your Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(['clinton','trump','sanders','cruz'])
